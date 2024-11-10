7c2fd3b31772c76f13db75fe06fc9bb8e4ababde
tomoking <arita.tomoki@keio.jp>
2024-11-11 02:10:16 +0900

    system statuslcm support

--- swarm_msgs/lcm_gen_msg.sh
+++ swarm_msgs/lcm_gen_msg.sh
@@ -8,6 +8,7 @@ lcm-gen msg/command_tol.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 lcm-gen msg/position_command.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 lcm-gen msg/goal_set.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 lcm-gen msg/odometry.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
+lcm-gen msg/system_status.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 
 # Pythonバインディングの生成（必要な場合）
 lcm-gen msg/indexed_odometry.lcm -p --ppath ./scripts/
@@ -15,3 +16,4 @@ lcm-gen msg/command_tol.lcm -p --ppath ./scripts/
 lcm-gen msg/position_command.lcm -p --ppath ./scripts/
 lcm-gen msg/goal_set.lcm -p --ppath ./scripts/
 lcm-gen msg/odometry.lcm -p --ppath ./scripts/
+lcm-gen msg/system_status.lcm -p --ppath ./scripts/
