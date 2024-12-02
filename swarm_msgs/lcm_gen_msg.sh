a9a7da1d6cb49c6bd280e4e167bc3fb9ee15441f
tomoking <arita.tomoki@keio.jp>
2024-12-02 21:45:11 +0900

    s

--- swarm_msgs/lcm_gen_msg.sh
+++ swarm_msgs/lcm_gen_msg.sh
@@ -9,6 +9,9 @@ lcm-gen msg/position_command.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 lcm-gen msg/goal_set.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 lcm-gen msg/odometry.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 lcm-gen msg/system_status.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
+lcm-gen msg/polynomial_trajectory.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
+lcm-gen msg/minco_traj.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
+lcm-gen msg/pointcloud2.lcm -x --cpp-hpath ./include/swarm_msgs/lcm_gen
 
 # Pythonバインディングの生成（必要な場合）
 lcm-gen msg/indexed_odometry.lcm -p --ppath ./scripts/
@@ -17,3 +20,6 @@ lcm-gen msg/position_command.lcm -p --ppath ./scripts/
 lcm-gen msg/goal_set.lcm -p --ppath ./scripts/
 lcm-gen msg/odometry.lcm -p --ppath ./scripts/
 lcm-gen msg/system_status.lcm -p --ppath ./scripts/
+lcm-gen msg/polynomial_trajectory.lcm -p --ppath ./scripts/
+lcm-gen msg/minco_traj.lcm -p --ppath ./scripts/
+lcm-gen msg/pointcloud2.lcm -p --ppath ./scripts/
